fa48e684c94787d07607001bfee2afd9ffc11d12
keetaek <keetaek@gmail.com>
2016-01-27 21:45:49 -0800

    Made modal more bootstrap.
    the buttons are still not wired.

--- src/containers/Checkin/Checkin.js
+++ src/containers/Checkin/Checkin.js
@@ -5,22 +5,11 @@ import { GuestList } from 'components';
 const Modal = require('react-modal');
 const customStyles = {
   overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(255, 255, 255, 0.75)',
     zIndex: 2
   },
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
+    padding: 0,
     zIndex: 3,
-    transform: 'translate(-50%, -50%)'
   }
 };
 export default class Checkin extends Component {
@@ -33,6 +22,7 @@ export default class Checkin extends Component {
   constructor(props) {
     super(props);
     this.state = { modalIsOpen: this.props.initialModalState };
+    require('./Checkin.scss');
   }
 
   checkinHandler(event) {
@@ -55,22 +45,6 @@ export default class Checkin extends Component {
       <div className="container">
         <h1>Check in</h1>
         <Helmet title="Check-in"/>
-          <Modal
-            isOpen={this.state.modalIsOpen}
-            onRequestClose={this.closeModal}
-            style={customStyles} >
-
-            <h2>Hello</h2>
-            <button onClick={this.closeModal}>close</button>
-            <div>I am a modal</div>
-            <form>
-              <input />
-              <button>tab navigation</button>
-              <button>stays</button>
-              <button>inside</button>
-              <button>the modal</button>
-            </form>
-          </Modal>
         <Grid>
           <Row className="show-grid">
             <Col xs={12} md={8}>
@@ -81,6 +55,32 @@ export default class Checkin extends Component {
             </Col>
           </Row>
         </Grid>
+        <Modal
+          className="Modal__Bootstrap modal-dialog"
+          closeTimeoutMS={150}
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.handleModalCloseRequest}
+          style={customStyles} >
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close" onClick={this.handleModalCloseRequest}>
+                <span aria-hidden="true">&times;</span>
+                <span className="sr-only">Close</span>
+              </button>
+              <h4 className="modal-title">Modal title</h4>
+            </div>
+            <div className="modal-body">
+              <h4>Really long content...</h4>
+              <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+              <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+              <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-default" onClick={this.handleModalCloseRequest}>Close</button>
+              <button type="button" className="btn btn-primary" onClick={this.handleSaveClicked}>Save changes</button>
+            </div>
+          </div>
+        </Modal>
       </div>
     );
   }
