15ad5072dfd1be5b66e16aece45fc6b5db1b5900
keetaek <keetaek@gmail.com>
2016-01-31 16:57:28 -0800

    Check content type
    For POST request, we need to ensure the incoming content type is what we expected. In this case, it will check whether it's a JSON file or not.
    Also adding RSVP to the package for future use.

---
+++ api/middlewares/checkContentType.js
@@ -0,0 +1,9 @@
+function checkContentType(req, res, next) {
+  const contype = req.headers['content-type'];
+  if (!contype || contype.indexOf('application/json') !== 0) {
+    return res.send(406).end();
+  }
+  return next();
+}
+
+module.exports = checkContentType
