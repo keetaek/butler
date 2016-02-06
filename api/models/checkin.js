46b57917025d5b0502de5220b2a916f6b0b5dc27
keetaek <keetaek@gmail.com>
2016-02-05 16:15:47 -0800

    Adding the foreign key guest_id to the checkin and locker models

--- api/models/checkin.js
+++ api/models/checkin.js
@@ -1,5 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   const Checkin = sequelize.define('Checkin', {
+    guest_id: {
+      type: DataTypes.INTEGER,
+      validate: {
+        notEmpty: true
+      }
+    },
     checkin_date: DataTypes.DATEONLY,
     feel_safe: DataTypes.BOOLEAN,
     heatlh_issue: DataTypes.BOOLEAN,
--- api/models/locker.js
+++ api/models/locker.js
@@ -1,5 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
   const Locker = sequelize.define('Locker', {
+    guest_id: {
+      type: DataTypes.INTEGER,
+      validate: {
+        notEmpty: true
+      }
+    },
     rent_start_date: DataTypes.DATEONLY,
     rent_end_date: DataTypes.DATEONLY,
     locker_number: DataTypes.STRING
