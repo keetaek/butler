33c02d57708e7fd300c36adec2268a547c9788ec
keetaek <keetaek@gmail.com>
2016-03-03 16:53:10 -0800

    Implemented loadCheckins method to fetch all the checkin info

--- src/redux/modules/checkin.js
+++ src/redux/modules/checkin.js
@@ -2,28 +2,31 @@ const RESET = 'butler/checkin/RESET';
 const CHECKIN = 'butler/checkin/CHECKIN';
 const CHECKIN_SUCCESS = 'butler/checkin/CHECKIN_SUCCESS';
 export const CHECKIN_FAIL = 'butler/checkin/CHECKIN_FAIL';
-const LOAD_DATE = 'butler/checkin/LOAD_DATE';
-const LOAD_DATE_SUCCESS = 'butler/checkin/LOAD_DATE_SUCCESS';
-const LOAD_DATE_FAIL = 'butler/checkin/LOAD_DATE_FAIL';
+const LOAD_CHECKINS = 'butler/checkin/LOAD_CHECKINS';
+const LOAD_CHECKINS_SUCCESS = 'butler/checkin/LOAD_CHECKINS_SUCCESS';
+export const LOAD_CHECKINS_FAIL = 'butler/checkin/LOAD_CHECKINS_FAIL';
 const START_CHECKIN = 'butler/checkin/START_CHECKIN';
 const FINISH_CHECKIN = 'butler/checkin/FINISH_CHECKIN';
+const DELETE_CHECKIN = 'butler/checkin/DELETE_CHECKIN';
+const DELETE_CHECKIN_SUCCESS = 'butler/checkin/DELETE_CHECKIN_SUCCESS';
+const DELETE_CHECKIN_FAIL = 'butler/checkin/DELETE_CHECKIN_FAIL';
 // const LOAD_GUEST_HISTORY = 'butler/checkin/LOAD_GUEST_HISTORY';
 // const LOAD_GUEST_HISTORY_SUCCESS = 'butler/checkin/LOAD_GUEST_HISTORY_SUCCESS';
 // const LOAD_GUEST_HISTORY_FAIL = 'butler/checkin/LOAD_GUEST_HISTORY_FAIL';
 // const UPDATE = 'butler/checkin/UPDATE';
 // const UPDATE_SUCCESS = 'butler/checkin/UPDATE_SUCCESS';
 // const UPDATE_FAIL = 'butler/checkin/UPDATE_FAIL';
-// const DELETE = 'butler/checkin/DELETE';
-// const DELETE_SUCCESS = 'butler/checkin/DELETE_SUCCESS';
-// const DELETE_FAIL = 'butler/checkin/DELETE_FAIL';
-const FEEL_SAFE_DEFAULT_VALUE = true;
-const HEALTH_ISSUE_DEFAULT_VALUE = false;
+const moment = require('moment');
+const { buildCheckinPayLoad, mapIncomingCheckins } =
+require('helpers/checkinDataMapper');
 
 const initialState = {
+  loading: false,
   loaded: false,
   showCheckinModal: false,
   selectedGuest: null,
-  notification: null
+  notification: null,
+  checkins: null
 };
 
 export default function reducer(state = initialState, action = {}) {
@@ -59,16 +62,34 @@ export default function reducer(state = initialState, action = {}) {
         selectedGuest: null,
         showCheckinModal: false,
       };
-    case LOAD_DATE:
+    case DELETE_CHECKIN:
       return {
-      };
-    case LOAD_DATE_SUCCESS:
-      return {
+        ...state,
 
       };
-    case LOAD_DATE_FAIL:
+    case LOAD_CHECKINS:
       return {
-
+        ...state,
+        loading: true,
+        loaded: false,
+      };
+    case LOAD_CHECKINS_SUCCESS:
+      const transformedData = mapIncomingCheckins(action.result);
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        checkins: transformedData
+      };
+    case LOAD_CHECKINS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        notification: {
+          status: LOAD_CHECKINS_FAIL,
+          data: action.error
+        }
       };
     case RESET:
       return initialState;
@@ -77,19 +98,8 @@ export default function reducer(state = initialState, action = {}) {
   }
 }
 
-function buildPayload(fields) {
-  return {
-    'guest_id': fields.id,
-    'feel_safe': fields.feelSafe || FEEL_SAFE_DEFAULT_VALUE,
-    'health_issue': fields.healthIssue || HEALTH_ISSUE_DEFAULT_VALUE,
-    'checkin_date': fields.date || new Date(),
-    'reported_items': fields.reportedItems || '',
-    'note': fields.note || ''
-  };
-}
-
 export function checkinGuest(fields) {
-  const payload = buildPayload(fields);
+  const payload = buildCheckinPayLoad(fields);
   return {
     types: [CHECKIN, CHECKIN_SUCCESS, CHECKIN_FAIL],
     promise: (client) => client.post('/checkins', {
@@ -106,17 +116,27 @@ export function startCheckin(guest) {
 export function finishCheckin() {
   return { type: FINISH_CHECKIN };
 }
-// function removeCheckin(guest) {
-//
-// }
-//
-// function updateCheckin(guest) {
-//
-// }
-//
-// function loadCheckins(date) {
-//
-// }
+
+export function deleteCheckin(guest) {
+  console.log('Need to delete checkin.')
+  // return { type: DELETE_CHECKIN, guest };
+}
+
+/**
+ * Load checkins for the given date.
+ * @param  {[Date]} date
+ */
+export function loadCheckins(start, end) {
+  const startDate = moment(start).format('YYYY-MM-DD');
+  let endDate = startDate;
+  if (end) {
+    endDate = moment(end).format('YYYY-MM-DD');
+  }
+  return {
+    types: [LOAD_CHECKINS, LOAD_CHECKINS_SUCCESS, LOAD_CHECKINS_FAIL],
+    promise: (client) => client.get('/checkins', { startDate: startDate, endDate: endDate })
+  };
+}
 //
 // function loadCheckins(guest) {
 //
