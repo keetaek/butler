33a4d2de16b8dd9d2f72c267936ebf8996553481
keetaek <keetaek@gmail.com>
2016-03-16 19:27:29 -0700

    Adding seeder function

---
+++ api/seeders/20160316190229-guests.js
@@ -0,0 +1,35 @@
+const Faker = require('faker');
+
+function createFakeData(numData) {
+  const guests = [];
+  for (var index = 0; index < numData; index++) {
+    guests.push({
+      first_name: Faker.name.firstName(),
+      last_name: Faker.name.lastName(),
+      nickname: Faker.name.firstName(),
+      birthdate: Faker.date.past(50, new Date('Sat Sep 20 1999 21:35:02 GMT+0200 (CEST)')),
+      gender: 'M',
+      emergency_contact_name: Faker.name.findName(),
+      emergency_contact_phone: Faker.phone.phoneNumber('###-###-####'),
+      identification_type: 'state',
+      identification_value: Faker.random.number(100000000),
+      identification_need_by: Faker.date.recent(),
+      identification_note: Faker.lorem.sentences(),
+      intake_form_collect_date: Faker.date.recent(),
+      intake_form_collected_by: Faker.name.findName()
+    });
+  }
+  return guests;
+}
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    const mockData = createFakeData(50);
+
+    return queryInterface.bulkInsert('Guests', mockData, {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Guests', null, {});
+  }
+};
